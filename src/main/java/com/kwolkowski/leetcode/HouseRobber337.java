81d91b355f6134f9468cf3d7117ca85d92d42a7e
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-12-05 21:51:38 +0100

    LC 337 - House Robber III - recursion + memo

--- src/main/java/com/kwolkowski/leetcode/HouseRobber337.java
+++ src/main/java/com/kwolkowski/leetcode/HouseRobber337.java
@@ -1,16 +1,25 @@
 package com.kwolkowski.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HouseRobber337 {
     public static int rob(TreeNode root) {
-        return Math.max(rob(root, true), rob(root, false));
+        return rob(root, new HashMap<>());
+    }
+    public static int rob(TreeNode root, Map<TreeNode, Integer> memo) {
+        if(memo.containsKey(root)) return memo.get(root);
+        int res = Math.max(rob(root, true, memo), rob(root, false, memo));
+        memo.put(root, res);
+        return res;
     }
 
-    public static int rob(TreeNode root, boolean rob) {
+    public static int rob(TreeNode root, boolean rob, Map<TreeNode, Integer> memo) {
         if(root == null) return 0;
         if(rob) {
-            return root.val + rob(root.left, false) + rob(root.right, false);
+            return root.val + rob(root.left, false, memo) + rob(root.right, false, memo);
         } else {
-            return rob(root.left) + rob(root.right);
+            return rob(root.left, memo) + rob(root.right, memo);
         }
     }
 
