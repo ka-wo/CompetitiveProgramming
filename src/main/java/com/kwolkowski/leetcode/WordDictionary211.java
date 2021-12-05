aad80acb32ab38587d8ca36ef62f8cad60f680e0
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2021-12-05 16:19:11 +0100

    LC 211 - Word Dictionary - Trie solution

--- src/main/java/com/kwolkowski/leetcode/WordDictionary211.java
+++ src/main/java/com/kwolkowski/leetcode/WordDictionary211.java
@@ -1,36 +1,57 @@
 package com.kwolkowski.leetcode;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class WordDictionary211 {
 
-    Map<Integer, Set<String>> words;
+    TrieNode trie;
 
     public WordDictionary211() {
-        words = new HashMap<>();
+        trie = new TrieNode();
     }
 
     public void addWord(String word) {
-        Set<String> set = words.getOrDefault(word.length(), new HashSet<>());
-        set.add(word);
-        words.put(word.length(), set);
+        TrieNode node = trie;
+
+        for (char ch : word.toCharArray()) {
+            if (!node.children.containsKey(ch)) {
+                node.children.put(ch, new TrieNode());
+            }
+            node = node.children.get(ch);
+        }
+        node.word = true;
     }
 
     public boolean search(String word) {
-        Set<String> strings = words.getOrDefault(word.length(), new HashSet<>());
-        outer: for (String s : strings) {
-            for (int i = 0; i < word.length(); i++) {
-                if(word.charAt(i) == '.' || word.charAt(i) == s.charAt(i)){
-                    continue;
-                } else {
-                    continue outer;
+        return search(word, trie);
+    }
+
+    private boolean search(String word, TrieNode curr) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.children.containsKey(c)) {
+                curr = curr.children.get(c);
+            } else {
+                if (c == '.') {
+                    for (Character character : curr.children.keySet()) {
+                        TrieNode child = curr.children.get(character);
+                        if (search(word.substring(i), child)) {
+                            return true;
+                        }
+                    }
                 }
+                return false;
             }
-            return true;
         }
-        return false;
+        return curr.word;
+    }
+
+    static class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean word = false;
+
+        public TrieNode() {
+        }
     }
 }
