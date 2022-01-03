e7ce9266f48d15ac0c5302982f7402a0bfbc997f
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2022-01-03 10:06:23 +0100

    LC 100 - same tree

---
+++ src/main/java/com/kwolkowski/leetcode/SameTree100.java
@@ -0,0 +1,31 @@
+package com.kwolkowski.leetcode;
+
+import java.util.LinkedList;
+
+public class SameTree100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
