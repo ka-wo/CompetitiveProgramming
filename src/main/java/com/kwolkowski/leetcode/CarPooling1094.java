90065c08b78e3285cb55b32e72557a9735432bb1
Kacper Wolkowski <kacper.wolkowski@gmail.com>
2022-01-06 16:18:08 +0100

    LC 1094 - Car Pooling - faster

--- src/main/java/com/kwolkowski/leetcode/CarPooling1094.java
+++ src/main/java/com/kwolkowski/leetcode/CarPooling1094.java
@@ -1,27 +1,14 @@
 package com.kwolkowski.leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.TreeMap;
-
 public class CarPooling1094 {
     public static boolean carPooling(int[][] trips, int capacity) {
-        Arrays.sort(trips, Comparator.comparingInt(a -> a[1]));
-        TreeMap<Integer, Integer> arrival = new TreeMap<>();
-
+        int[] road = new int[1001];
         for (int[] trip : trips) {
-            int numPass = trip[0];
-            int from = trip[1];
-            int to = trip[2];
-            Integer arrived = arrival.floorKey(from);
-            while(arrived != null) {
-                capacity += arrival.remove(arrived);
-                arrived = arrival.floorKey(from);
-            }
-            capacity -= numPass;
-            Integer count = arrival.getOrDefault(to, 0);
-            arrival.put(to, numPass + count);
-
+            road[trip[1]] += trip[0];
+            road[trip[2]] -= trip[0];
+        }
+        for (int i : road) {
+            capacity -= i;
             if(capacity < 0) return false;
         }
         return true;
